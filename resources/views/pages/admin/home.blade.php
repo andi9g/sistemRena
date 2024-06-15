a65fa15b6d98e78a63a60d3313d334e7980c4011
andi9g <andirizky.bayuputra@gmail.com>
2024-06-15 21:08:19 +0700

    oke

--- resources/views/pages/admin/home.blade.php
+++ resources/views/pages/admin/home.blade.php
@@ -16,11 +16,7 @@
                         TOTAL KEUANGAN
                     </div>
                 </div>
-                <div class="widget-icon">
-                    <div class="icon" data-color="#00eccf" style="color: rgb(0, 236, 207);">
-                        <i class="icon-copy dw dw-calendar1"></i>
-                    </div>
-                </div>
+
             </div>
         </div>
     </div>
@@ -33,11 +29,7 @@
                         TOTAL PEMASUKAN KAS
                     </div>
                 </div>
-                <div class="widget-icon">
-                    <div class="icon" data-color="#ff5b5b" style="color: rgb(255, 91, 91);">
-                        <span class="icon-copy ti-heart"></span>
-                    </div>
-                </div>
+
             </div>
         </div>
     </div>
@@ -50,11 +42,7 @@
                         TOTAL PEMASUKAN TAMBAHAN
                     </div>
                 </div>
-                <div class="widget-icon">
-                    <div class="icon">
-                        <i class="icon-copy fa fa-stethoscope" aria-hidden="true"></i>
-                    </div>
-                </div>
+
             </div>
         </div>
     </div>
@@ -65,11 +53,7 @@
                     <div class="weight-700 font-24 text-dark">Rp{{ number_format($pengeluaran, 0, ",", ".") }}</div>
                     <div class="font-14 text-secondary weight-500">TOTAL PENGELUARAN</div>
                 </div>
-                <div class="widget-icon">
-                    <div class="icon" data-color="#09cc06" style="color: rgb(9, 204, 6);">
-                        <i class="icon-copy fa fa-money" aria-hidden="true"></i>
-                    </div>
-                </div>
+
             </div>
         </div>
     </div>
